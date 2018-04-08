ad749a4a15b7421eafd6f8b71830e5e904b20755
joe2459 <jlabanca1997@msn.com>
2018-04-08 15:39:15 -0400

    My commit message

--- app/src/main/java/com/bignerdranch/android/personaltrainer/LoginFragment.java
+++ app/src/main/java/com/bignerdranch/android/personaltrainer/LoginFragment.java
@@ -1,13 +1,19 @@
 package com.bignerdranch.android.personaltrainer;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
@@ -22,6 +28,53 @@ public class LoginFragment extends Fragment {
     private Login mName;
 
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+    super.onCreateOptionsMenu(menu, inflater);
+    inflater.inflate(R.menu.fragment_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.log_off:
+                //start dialog
+                //Intent intent = new Intent(getActivity(), LoginActivity.class);
+                AlertDialog.Builder logoff = new AlertDialog.Builder(getActivity());
+                logoff.setMessage("Are you sure you want to log out?")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Intent intent = new Intent(getActivity(),LoginActivity.class);
+                                startActivity(intent);
+                            }
+                        })
+                        .setNegativeButton("No",new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        }) ;
+
+                AlertDialog alert = logoff.create();
+                alert.setTitle(R.string.logoff_title);
+                alert.show();
+
+                return(true);
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
